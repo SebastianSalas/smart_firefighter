e8b7a001207179b0ee3c5b4675188c92b553a250
SebastianSalas <janiersalas09@gmail.com>
2023-09-30 14:20:45 -0500

    Order project structure

--- IA_project.py
+++
@@ -1,9 +0,0 @@
-with open("mapa.txt", "r") as file:
-    file_map = file.read()
-
-file_map = file_map.replace('[', '')
-file_map = file_map.replace(']', '')
-
-map = [int(n) for n in file_map.split(',')]
-
-print(map)
---
+++ main.py
@@ -0,0 +1,11 @@
+class SmartFirefighter:
+  def main(self):
+    with open("resources/map.txt", "r") as file:
+        file_map = file.read()
+
+    map = [int(n) for n in file_map.split(',')]
+
+    print(map)
+    
+if __name__ == "__main__":
+    SmartFirefighter().main()
