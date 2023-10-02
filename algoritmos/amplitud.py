0af88f89aa23369c25a548c0fae5ab314d4912c1
SebastianSalas <janiersalas09@gmail.com>
2023-10-02 00:48:55 -0500

    Verify nodos_visitados

--- algoritmos/amplitud.py
+++ algoritmos/amplitud.py
@@ -1,17 +1,15 @@
 from collections import deque
 
-def verificarNodoVisitado(map,nodos):
-  for i in range(map.shape[0]):  # filas
-      for j in range(map.shape[1]): #columnas
-        return [i,j] in nodos
+def verificarNodoVisitado(nodo, nodos_visitados):
+  return nodo in nodos_visitados
   
 def verificarDentroDelMapa(nodo, map):
   return 0 <= nodo[0] < map.shape[0] and 0 <= nodo[1] < map.shape[1]
     
-def retornarHijos(nodo_padre,map):
+def retornarHijos(nodo_padre,map, nodos_visitados):
   lista_hijos=[]
   for n in list(([nodo_padre[0]+1,nodo_padre[1]], [nodo_padre[0]-1,nodo_padre[1]], [nodo_padre[0],nodo_padre[1]+1], [nodo_padre[0],nodo_padre[1]-1])):
-          if verificarDentroDelMapa(n,map):
+          if verificarDentroDelMapa(n,map) and not(verificarNodoVisitado(n,nodos_visitados)):
             lista_hijos.append(n)
   return lista_hijos
 
@@ -23,7 +21,7 @@ def amplitud(map):
     for j in range(map.shape[1]): #columnas
       if(map[i][j] == 5):
         nodos_visitados.append([i,j])
-        nodos_hijos = retornarHijos([i,j], map)
+        nodos_hijos = retornarHijos([i,j], map, nodos_visitados)
         
         
       
