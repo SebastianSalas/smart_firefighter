4caed3e14930161d8aff0e6ab4a0473235d5c004
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-10-06 22:49:51 -0500

    adds solve function for costo_uniforme

--- algoritmos/costo_uniforme.py
+++ algoritmos/costo_uniforme.py
@@ -1,4 +1,5 @@
 import copy
+from collections import deque
 # Costo para cada suministro
 costo_general = 1
 cubeta_un_litro = 1 + costo_general
@@ -23,14 +24,14 @@ class costo_uniforme():
 
     def __init__(self, mapa):
         self.map = mapa
-        
+
     def movimiento_valido(self, new_x, new_y, nuevo_mapa):
         """
         Verifica si un movimiento a las coordenadas (new_x, new_y) es válido en el mapa n_mapa.
         Debe devolver True si el movimiento es válido y False si no lo es.
         """
         return (0 <= new_x < len(nuevo_mapa)) and (0 <= new_y < len(nuevo_mapa[0])) and nuevo_mapa[new_x][new_y] != 1
-    
+
     def nueva_posicion(self, nodo, direccion):
         """
         Calcula las nuevas coordenadas (new_x, new_y) en función de la dirección.
@@ -61,7 +62,7 @@ class costo_uniforme():
             if self.movimiento_valido(new_x, new_y, nuevo_mapa):
                 nodos_hijos.append(self.state(nodo, new_x, new_y))
         return nodos_hijos
-    
+
     def nodos_vecinos(self, nodo, n_mapa):
         x, y = nodo.posicion
         neighbors = []
@@ -72,10 +73,11 @@ class costo_uniforme():
             child = self.state(nodo, new_x, new_y)
             neighbors.append(child)
         return neighbors
+
     def estado_padre(self, node, direccion):
         if node.padre is None:
             return True
-    
+
         parent_x, parent_y = node.padre.posicion
         padre_cubetas, padre_fuego = node.padre.cubetas, node.padre.punto_fuego
         x, y = node.posicion
@@ -89,10 +91,11 @@ class costo_uniforme():
         if direccion == 'right' and parent_y == y + 1:
             return not (padre_cubetas == node.cubetas and padre_fuego == node.punto_fuego)
         return True
-    def state(self, node, position_x, position_y):
-        
+
+    def state(self, node, pos_x, pos_y):
+
         map = copy.deepcopy(node.mapa)
-        tipo_celda = map[position_x][position_y]
+        tipo_celda = map[pos_x][pos_y]
         cost = costo_general
 
         if tipo_celda == 3:  # cubeta de un litro
@@ -100,7 +103,49 @@ class costo_uniforme():
         elif tipo_celda == 4:  # cubeta de dos litros
             cost += cubeta_dos_litro if node.cubetas == 0 else costo_general
 
-        map[position_x][position_y] = 0
-        child = Nodo(node, (position_x, position_y), map, node.cubetas, node.punto_fuego, node.cost + cost)
+        map[pos_x][pos_y] = 0
+        child = Nodo(node, (pos_x, pos_y), map,
+                     node.cubetas, node.punto_fuego, node.costo + cost)
 
         return child
+
+    def solucion_al(self):
+        '''
+        resuelve el problema de busqueda utilizando el algoritmo de busqueda por amplitud.
+        '''
+        pos_inicial = None
+        nodos_exp = 0
+        
+        for i in range(len(self.mapa)):
+            for j in range(len(self.mapa)):
+                if self.mapa[i][j] == 5:
+                    pos_inicial = (i, j)
+       
+        nodo_inicial = Nodo(None, pos_inicial, self.mapa, 0, 0)
+        queue = deque([nodo_inicial])
+        finalizado = False
+
+        while not finalizado:
+            nodo_actual = queue.popleft() 
+            if nodo_actual.punto_fuego == 2:
+                finalizado = True
+            else:
+                nodos_exp += 1
+            queue.extend(self.expand_node(nodo_actual, self.mapa))
+
+        solucion = nodo_actual
+        camino = []   # camino recorrido
+        mapas = []   # los mapas de cada nodo
+
+        while solucion.padre is not None:
+            camino.append(solucion.posicion)
+            mapas.append(solucion.mapa)
+            solucion = solucion.padre
+
+        camino.append(solucion.posicion)
+        camino.reverse()
+
+        mapas.append(solucion.mapa)
+        mapas.reverse()
+
+        return camino, nodos_exp, mapas, nodo_actual.costo
