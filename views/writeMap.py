87d2b8423eae4548c97b687370505eb4accaa4e3
SebastianSalas <janiersalas09@gmail.com>
2023-10-11 22:16:51 -0500

    adds more code

--- views/writeMap.py
+++ views/writeMap.py
@@ -1,10 +1,10 @@
 import numpy as np
-import algoritmos.amplitud as amplitud
+import amplitud as amplitud
 
 class writeMap:
-    with open("resources/map.txt", "r") as file:
+    with open("../resources/map.txt", "r") as file:
       file_map = file.readlines()
 
       map = np.array([list(map(int, n.split(' '))) for n in file_map])
-      amplitud.amplitud(map)
+      amplitud.solve(map)
       
