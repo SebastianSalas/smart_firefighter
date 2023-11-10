714723c6a5694dbfba1f6f2ec698eb70ccecdd6e
AlexanderValencia21 <alexandervalencia0121@gmail.com>
2023-11-10 15:06:15 -0500

    fix avara error

--- views/avara.py
+++ views/avara.py
@@ -2,33 +2,47 @@ import time
 import numpy as np
 from NodeInformedSearch import Node
 from collections import deque
+from scipy.spatial import distance
 
 
-def heuristic_fun(nodo, water_positions, fire_positions, bucket_positions):
-    closest_bucket = min([((nodo.position[0] - bucket[0]) ** 2 + (
-        nodo.position[1] - bucket[1]) ** 2) ** 0.5 for bucket in bucket_positions])
-    closest_water = min([((nodo.position[0] - water[0]) ** 2 +
-                        (nodo.position[1] - water[1]) ** 2) ** 0.5 for water in water_positions])
-    closest_fire = min([((nodo.position[0] - fire[0]) ** 2 +
-                       (nodo.position[1] - fire[1]) ** 2) ** 0.5 for fire in fire_positions])
+
+def euclidean_distance(point1, point2):
+    return np.linalg.norm(np.array(point2) - np.array(point1))
+
+def heuristic_fun(nodo):
+    nodo_position = np.array(nodo.position).reshape(1, -1)
+
     if not nodo.bucket1 and not nodo.bucket2:
-        # Calcular la distancia a la cubeta más cercana
-        return closest_bucket
+        bucket1_positions = np.array(np.where(nodo.map == 3)).T
+        bucket2_positions = np.array(np.where(nodo.map == 4)).T
+        
+        if bucket1_positions.size > 0 and bucket2_positions.size > 0:
+            b1 = euclidean_distance(nodo_position, np.ravel(bucket1_positions)[0])
+            b2 = euclidean_distance(nodo_position, np.ravel(bucket2_positions)[0])
+            if b1 <= b2:
+                return b1
+            else:
+                return b2
+
     if (nodo.bucket1 or nodo.bucket2) and nodo.water_q == 0:
-        # Calcular la distancia al punto de agua más cercano
-        return closest_water
+        water_positions = np.array(np.where(nodo.map == 6)).T
+        return euclidean_distance(nodo_position, np.ravel(water_positions)[0])
+
     if (nodo.bucket1 or nodo.bucket2) and nodo.water_q > 0:
-        # Calcular la distancia al punto de fuego más cercano
-        return closest_fire
+        fire_positions = np.array(np.where(nodo.map == 2)).T
+        return euclidean_distance(nodo_position, np.ravel(fire_positions)[0])
+
     return 0
+
 
 
 def Goal(nodo):
     return nodo.map[nodo.position[0], nodo.position[1]] in [2, 3, 4, 6]
 
 
-def checkFinished(nodo,count_fire):
-  return nodo.fire_extinguished == count_fire-1 and nodo.map[nodo.position[0], nodo.position[1]] in [2]
+def checkFinished(nodo, count_fire):
+    return nodo.fire_extinguished == count_fire-1 and nodo.map[nodo.position[0], nodo.position[1]] in [2]
+
 
 def verifyMap(nodo, position):
     if 0 <= position[0] < nodo.map.shape[0] and 0 <= position[1] < nodo.map.shape[1] and nodo.map[position[0], position[1]] != 1:
@@ -88,28 +102,22 @@ def checkParent(nodo, operator):
     return True
 
 
-def checkMovimiento(nodo, nodos_e, fire_positions, water_positions, bucket_positions):
+def checkMovimiento(nodo, nodos_e):
     child_list = []
     pos_x = nodo.position[0]
     pos_y = nodo.position[1]
     expanded_nodes = nodos_e
-    heuristic_value = heuristic_fun(
-        nodo, water_positions, fire_positions, bucket_positions)
     if verifyMap(nodo, [pos_x, pos_y+1]) and checkParent(nodo, 2):
         child, expanded_nodes = verifyGoal(nodo, pos_x, pos_y+1, nodos_e, 2)
-        child.heuristic = heuristic_value  # Asigna la heurística calculada
         child_list.append(child)
     if verifyMap(nodo, [pos_x-1, pos_y]) and checkParent(nodo, 0):
         child, expanded_nodes = verifyGoal(nodo, pos_x-1, pos_y, nodos_e, 0)
-        child.heuristic = heuristic_value  # Asigna la heurística calculada
         child_list.append(child)
     if verifyMap(nodo, [pos_x+1, pos_y]) and checkParent(nodo, 1):
         child, expanded_nodes = verifyGoal(nodo, pos_x+1, pos_y, nodos_e, 1)
-        child.heuristic = heuristic_value  # Asigna la heurística calculada
         child_list.append(child)
     if verifyMap(nodo, [pos_x, pos_y-1]) and checkParent(nodo, 3):
         child, expanded_nodes = verifyGoal(nodo, pos_x, pos_y-1, nodos_e, 3)
-        child.heuristic = heuristic_value  # Asigna la heurística calculada
         child_list.append(child)
 
     return child_list, expanded_nodes
@@ -119,49 +127,49 @@ def verifyGoal(nodo, pos_x, pos_y, nodos_e, operator):
     expanded_nodes = nodos_e
     water_temp = nodo.water_q
     nodo_map = (nodo.map).copy()
-    if nodo_map[nodo.position[0], nodo.position[1]] == 3:
+    if nodo_map[nodo.position[0], nodo.position[1]] == 3:  # 1l
         new_map = np.where(np.logical_or(
             nodo_map == 3, nodo_map == 4), 0, nodo_map)
         node_child = Node(nodo, operator, [pos_x, pos_y], new_map, True, nodo.bucket2,
-                          nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, True)
+                          nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, nodo.change_state,heuristic_fun(nodo))
         expanded_nodes += 1
-    elif nodo_map[nodo.position[0], nodo.position[1]] == 4:
+    elif nodo_map[nodo.position[0], nodo.position[1]] == 4:  # 2l
         new_map = np.where(np.logical_or(
             nodo_map == 3, nodo_map == 4), 0, nodo_map)
-        node_child = Node(nodo, operator, [pos_x, pos_y], new_map, nodo.bucket1,
-                          True, nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, True)
+        node_child = Node(nodo, operator, [pos_x, pos_y], new_map, nodo.bucket1, True,
+                          nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
         expanded_nodes += 1
-    elif nodo_map[nodo.position[0], nodo.position[1]] == 2:
+    elif nodo_map[nodo.position[0], nodo.position[1]] == 2:  # fire
         if nodo.bucket1 and water_temp > 0:
             nodo_map[nodo.position[0], nodo.position[1]] = 0
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished + 1, nodo.water_q - 1, nodo.depth + 1, True)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished + 1, nodo.water_q - 1, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
             expanded_nodes += 1
         elif nodo.bucket2 and water_temp > 0:
             nodo_map[nodo.position[0], nodo.position[1]] = 0
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished + 1, nodo.water_q - 1, nodo.depth + 1, True)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished + 1, nodo.water_q - 1, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
             expanded_nodes += 1
         else:
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, False)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
             expanded_nodes += 1
-    elif nodo_map[nodo.position[0], nodo.position[1]] == 6:
+    elif nodo_map[nodo.position[0], nodo.position[1]] == 6:  # water
         if nodo.bucket1 and nodo.water_q == 0:
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished, nodo.water_q + 1, nodo.depth + 1, True)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished, nodo.water_q + 1, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
             expanded_nodes += 1
         elif nodo.bucket2 and nodo.water_q == 0:
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished, nodo.water_q + 2, nodo.depth + 1, True)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished, nodo.water_q + 2, nodo.depth + 1, nodo.change_state,heuristic_fun(nodo))
             expanded_nodes += 1
         else:
-            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                              nodo.bucket2, nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, False)
+            node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                              nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
             expanded_nodes += 1
     else:
-        node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1,
-                          nodo.bucket2, nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, False)
+        node_child = Node(nodo, operator, [pos_x, pos_y], nodo_map, nodo.bucket1, nodo.bucket2,
+                          nodo.fire_extinguished, nodo.water_q, nodo.depth + 1, nodo.change_state, heuristic_fun(nodo))
         expanded_nodes += 1
 
     return node_child, expanded_nodes
@@ -180,11 +188,8 @@ def solve(map):
             if (map[i][j] == 5):
                 pos_i = [i, j]
 
-    nodo_i = Node(None, None, pos_i, map, False, False, 0, 0, 0, False, 0, 0)
+    nodo_i = Node(None, None, pos_i, map, False, False, 0, 0, 0)    
     stack.append(nodo_i)
-    fire_positions = [[4, 3], [5, 7]]
-    bucket_positions = [[1, 7], [5, 5]]
-    water_position = [[4, 5]]
     count_fire = np.count_nonzero(map == 2)
 
     while not finished:
@@ -197,8 +202,8 @@ def solve(map):
             end_time = time.time()
         else:
             children_nodes, expanded_nodes = checkMovimiento(
-                current_node, expanded_nodes, water_position, fire_positions, bucket_positions)
-            stack.extendleft(reversed(children_nodes))
+                current_node, expanded_nodes)
+            stack.extend(children_nodes)
 
     path = []
     list_heu = []
